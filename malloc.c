062a5078fa5cb003323aae54313a1eee6173ad95
Helder Doutel <helder.gomes@nube.com.br>
2018-08-24 17:04:44 -0300

    lista duplamente encadeada

--- malloc.c
+++ malloc.c
@@ -2,6 +2,7 @@
 #include<stddef.h>
 static unsigned char memoria[1024*1024];
 
+// malloc
 struct bloco{
     size_t tamanho;
     int free;
@@ -54,12 +55,12 @@ void* aloca(size_t tamanho){
 
 void print_memoria(){
     struct bloco *atual;
-    atual   = livre;
+    atual = livre;
     printf("inicio\n");
     while(1==1){
         printf("Tamanaho: %lu\n", atual->tamanho);
         printf("Endereco: %d\n", atual);
-        printf("livre: %d\n", atual-livre);
+        printf("livre: %d\n", atual->free);
         printf("proximo: %d\n", atual->proximo);
         printf("\n\n", atual->proximo);
         if(atual->proximo==NULL){
@@ -94,7 +95,49 @@ void liberar(void *liberar){
     }
 }
 
+
+// lista duplamente encadeada
+struct lenc{
+    int valor;
+    struct lenc *proximo;
+    struct lenc *anterior;
+};
+
+void* criar_lenc(int valor){
+    struct lenc *novo = aloca(sizeof(struct lenc));
+    novo->proximo = NULL;
+    novo->anterior = NULL;
+    novo->valor = valor;
+    return novo;
+}
+
+void inserir_fim_lenc(struct lenc *atual, int valor){
+    struct lenc *novo = aloca(sizeof(struct lenc));
+    struct lenc *fim;
+    fim = atual;
+    while(fim->proximo != NULL){
+        fim = fim->proximo;
+    }
+    fim->proximo = novo;
+    novo->valor = valor;
+    novo->anterior = fim;
+}
+
+void print_lenc(struct lenc *usado){
+    struct lenc *atual = usado;
+    while(1==1){
+        printf("valor: %d\n", atual->valor);
+        if(atual->proximo==NULL){
+            break;
+        }
+        atual=atual->proximo;
+        
+    }
+}
+
+// main
 int main(){
+    struct lenc *lista_enc;
     print_memoria();
     int *teste = (int*) aloca(sizeof(int));
     print_memoria();
@@ -107,7 +150,12 @@ int main(){
     printf("%d %d\n", *teste, *teste2);
     liberar(teste);
     liberar(teste2);
+    printf("encadeada\n");
     print_memoria();
-    
+    lista_enc = criar_lenc(10);
+    inserir_fim_lenc(lista_enc, 1);
+    inserir_fim_lenc(lista_enc, 2);
+    print_memoria();
+    print_lenc(lista_enc);
     return 0;   
 }
